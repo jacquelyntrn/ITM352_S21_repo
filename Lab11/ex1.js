c19ac3ca335084d06ef169088e67fb27bf7a9bd5
jacquelyntrn <jtvtran@hawaii.edu>
2021-03-11 13:17:17 -1000

    started lab11

---
+++ Lab11/ex1.js
@@ -0,0 +1,22 @@
+console.log('Hello world');
+attributes  =  "Jackie;19;ACC";
+info = attributes.split(';');
+//console.log (info);
+
+for(part of info) {
+    console.log(isNonNegInt(info));
+}
+
+function isNonNegInt (q, returnErrors=false) {
+errors = []; // assume no errors at first
+if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+return returnErrors ? errors : (errors.length == 0)
+}
+
+isNonNegInt('1');
+isNonNegInt('-1');
+isNonNegInt('.19');
+isNonNegInt('word');
