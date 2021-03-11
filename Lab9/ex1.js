e4e73a2e441dad1abde30fa48290e9f9ae9cf47e
jacquelyntrn <jtvtran@hawaii.edu>
2021-03-11 12:23:02 -1000

    og

--- Lab9/ex1.js
+++ Lab9/ex1.js
@@ -1,3 +1,13 @@
-m=3
-d=3
-y=2001
+// Robot moving sim thing
+// Randomly moves around the box
+// You're welcome whoever is reading this
+while (0 < 1) {
+    var x = Math.floor((Math.random() * 10) + 1);
+
+    if (x < 7) {
+        controller.move();
+    }
+    else {
+        controller.rotate();
+    }
+}
