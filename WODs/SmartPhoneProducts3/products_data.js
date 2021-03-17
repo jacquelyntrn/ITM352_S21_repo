61d244030bbcf759359402abea2f4d8d7a02bad5
jacquelyntrn <jtvtran@hawaii.edu>
2021-03-16 19:19:16 -1000

    object and array for products

---
+++ WODs/SmartPhoneProducts3/products_data.js
@@ -0,0 +1,24 @@
+//object template var product = {brand:"", price:, image:};
+//array template for all products var products = [product1, product2, ...];
+//Product info
+products = [
+product1 = { name: "HTC",
+ price: 40.00,
+ image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"},
+
+product2 = { name: "Apple",
+ price: 75.00,
+ image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"},
+
+product3 = { name: "Nokia",
+ price: 35.00,
+ image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"},
+
+product4 = { name: "Samsung",
+ price: 45.00,
+ image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"},
+
+product5 = { name: "Blackberry",
+ price: 10.00,
+ image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"},    
+]
