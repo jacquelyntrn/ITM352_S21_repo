17fea911928eeb54306858dcdc7317403a8e1cbb
jacquelyntrn <jtvtran@hawaii.edu>
2021-03-22 01:55:48 -1000

    trying lab11

--- Lab11/ex1.js
+++ Lab11/ex1.js
@@ -1,22 +1,14 @@
-console.log('Hello world');
-attributes  =  "Jackie;19;ACC";
-info = attributes.split(';');
+attributes1  =  "Jackie;19;ACC";
+attributes2  =  "Jackie;20;20.5;19.5";
+//info = attributes.split(';');
 //console.log (info);
 
-for(part of info) {
-    console.log(isNonNegInt(info));
+function myName (pieces) {
+    return pieces.split(';');
 }
 
-function isNonNegInt (q, returnErrors=false) {
-errors = []; // assume no errors at first
-if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
-if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+console.log (myName(attributes2));
 
-return returnErrors ? errors : (errors.length == 0)
+for (i = 0; i < 3; i++) {
+    console.log(pieces[i]);
 }
-
-isNonNegInt('1');
-isNonNegInt('-1');
-isNonNegInt('.19');
-isNonNegInt('word');
