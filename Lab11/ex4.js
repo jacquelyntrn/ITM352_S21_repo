6ebaa99c2c6d4d206dfbd7e19e0315acc825bea2
jacquelyntrn <jtvtran@hawaii.edu>
2021-03-23 13:51:37 -1000

    lab12

--- Lab11/ex4.js
+++ Lab11/ex4.js
@@ -1,6 +1,6 @@
 age = 20;
-name = "Jackie";
-attributes = name + ";" + age + ";" + (age + 0.5) + ";" + (0.5 - age);
+myname = "Jackie";
+attributes = myname + ";" + age + ";" + (age + 0.5) + ";" + (0.5 - age);
 pieces = attributes.split(';');
 
 function isNonNegInt (q, returnErrors=false) {
@@ -9,8 +9,19 @@ if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number 
 if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
 if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
 
-return returnErrors ? errors : (errors.length == 0)
+return returnErrors ? errors : (errors.length == 0) //returns an array of all the errors
 }
+
+function checkNum(item, index) {
+    returnErrors = isNonNegInt(item, true).join(" ");
+    if (returnErrors.length == 0) {
+        console.log("\'" + item + "' is valid");
+    } else {
+    console.log("'" + item + "' is " + returnErrors);
+    }
+}
+
+pieces.forEach(checkNum);
 
 console.log(pieces)
 
@@ -18,7 +29,7 @@ for (i in pieces) {
     console.log (isNonNegInt (pieces[i]));
 }
 
-console.log (isNonNegInt('20'));
+//console.log (isNonNegInt('20'));
 /*isNonNegInt('-1');
 isNonNegInt('.19');
 isNonNegInt('word');*/
