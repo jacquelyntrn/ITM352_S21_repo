3331c7ede3804db756a48b7bdb5e5b7da1beffbc
jacquelyntrn <jtvtran@hawaii.edu>
2021-04-05 15:41:31 -1000

    idk
    lab13 stuff

---
+++ Lab13/info_server_Ex2c.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const QueryString = require('qs');
+
+var app = express();
+
+//the asterick can mean anything that is in the same format
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+
+app.get('/test', function (request, response, next) {
+    response.send(request.method + ' to path ' + request.path + ` with query` + JSON.stringify(QueryString));
+});
+
+app.listen8080, function() {
+    console.log(`listening on port 8080`)
+};
+
+app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
+
+//use nodemon, similar to live server and will automatically restart your server.
